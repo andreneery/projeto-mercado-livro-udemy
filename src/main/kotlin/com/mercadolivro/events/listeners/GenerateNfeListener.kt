15d36ebeb826d115ffd95904608b7380d507679f
André Nery <andre.nery@creditas.com>
2023-01-31 19:30:43 -0300

    alterando o eventos para que sejam assincronos, ou seja, os eventos apoós finalizado o

--- src/main/kotlin/com/mercadolivro/events/listeners/GenerateNfeListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listeners/GenerateNfeListener.kt
@@ -3,6 +3,7 @@ package com.mercadolivro.events.listeners
 import com.mercadolivro.events.PurchaseEvents
 import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.UUID
 
@@ -10,6 +11,7 @@ import java.util.UUID
 class GenerateNfeListener(
     private val purchaseService: PurchaseService
 ) {
+    @Async
     @EventListener
     fun listen(purchaseEvents: PurchaseEvents){
         val nfe = UUID.randomUUID().toString()
--- src/main/kotlin/com/mercadolivro/events/listeners/UpdateSoldBookListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listeners/UpdateSoldBookListener.kt
@@ -4,6 +4,7 @@ import com.mercadolivro.events.PurchaseEvents
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.UUID
 
@@ -11,6 +12,7 @@ import java.util.UUID
 class UpdateSoldBookListener(
     private val bookService: BookService
 ) {
+    @Async
     @EventListener
     fun listen(purchaseEvents: PurchaseEvents){
 
